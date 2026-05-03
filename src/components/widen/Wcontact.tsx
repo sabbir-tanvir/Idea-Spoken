e8d2cf8ed20c1b60935ec1cf5b7eec00c48708be
Sabbir Tanvir <106466798+sabbir-tanvir@users.noreply.github.com>
2026-05-03 19:33:22 +0600

    add fb link

--- src/components/widen/Wcontact.tsx
+++ src/components/widen/Wcontact.tsx
@@ -4,13 +4,14 @@ import ContactCard from '../ui/cards/ContactCard';
 const Wcontact = () => {
   return (
     <ContactCard
-      title="Join The WIDEN Community"
+      title="Join The WINI Community"
       subtitle="আমাদের গ্রুপে যোগ দিন এবং আমাদের উদ্যোগগুলো সম্পর্কে জানুন"
       buttonLabel="Join WIDEN Group"
-      buttonHref="https://www.facebook.com/groups/widen"
+      buttonHref="https://web.facebook.com/groups/2908320882628536"
       buttonTarget="_blank"
     />
   );
 };
 
 export default Wcontact;
+  
