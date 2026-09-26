6ea38c2d72568784376254ca28475d92646817a0
Sabbir Tanvir <106466798+sabbir-tanvir@users.noreply.github.com>
2026-09-26 14:23:51 +0600

    wini

--- src/components/pitha/PContact.tsx
+++ src/components/pitha/PContact.tsx
@@ -7,7 +7,7 @@ const Wcontact = () => {
       title="অর্থায়ন, ফ্র্যাঞ্চাইজি বা পার্টনারশিপে আগ্রহী?"
       subtitle="আমাদের সাথে যোগাযোগ করুন এবং এই উদ্যোগের অংশ হন"
       buttonLabel="Contact for Franchise / Partnership"
-      buttonHref="https://www.facebook.com/groups/widen"
+      buttonHref="https://www.facebook.com/groups/wini"
       buttonTarget="_blank"
     />
   );
